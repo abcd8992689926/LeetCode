76b9078c1181a94a8c3e8209c8d7dc5f8d25be09
Felix <abcd8992689926@gmail.com>
2023-03-09 09:35:26 +0800

    Modify Answer

--- 1109.corporate-flight-bookings.cs
+++ 1109.corporate-flight-bookings.cs
@@ -9,17 +9,19 @@ public class Solution {
     public int[] CorpFlightBookings(int[][] bookings, int n) {
         var diff=new int[n];
         diff[0]=0;
-        for(var x=0; x<n; ++x){
+        for(var x=0; x<bookings.Length; ++x){
             var i=bookings[x][0]-1;
             var j=bookings[x][1];
             var v=bookings[x][2];
             diff[i]+=v;
-            diff[j]-=v;
+            if(j<n){
+                diff[j]-=v;
+            } 
         }
         var r=new int[n];
         r[0]=diff[0];
         for(var x=1;x<n;++x){
-            r[1]=r[0]+diff[x];
+            r[x]=r[x-1]+diff[x];
         }
         return r;
     }
