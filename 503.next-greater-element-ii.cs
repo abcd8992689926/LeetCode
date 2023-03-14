bc64c063187a0529b2093ef701860299b0899710
Felix <abcd8992689926@gmail.com>
2023-03-14 21:54:30 +0800

    Create Branch

---
+++ 503.next-greater-element-ii.cs
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode id=503 lang=csharp
+ *
+ * [503] Next Greater Element II
+ */
+
+// @lc code=start
+public class Solution {
+    public int[] NextGreaterElements(int[] nums) {
+        
+    }
+}
+// @lc code=end
+
