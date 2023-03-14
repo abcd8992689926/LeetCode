996a279a4a64a0969375e935ae498aa471c788e3
Felix <abcd8992689926@gmail.com>
2023-03-14 22:37:13 +0800

    First Try

--- 503.next-greater-element-ii.cs
+++ 503.next-greater-element-ii.cs
@@ -6,9 +6,27 @@
 
 // @lc code=start
 public class Solution {
+    private Queue<int> queue=new Queue<int>();
     public int[] NextGreaterElements(int[] nums) {
-        
+        var r=new List<int>();
+        queue.Enqueue(nums[0]);
+        for(var x=1; x<nums.Length; ++x){
+            while(queue.Peek()<nums[x]){
+                queue.Dequeue();
+            }
+            if(queue.Count==0){
+                queue.Enqueue(nums[x]);
+            }
+            if(x!=nums.Length-1&&nums[x]>nums[x+1]){
+                r.Add(-1);
+            }else{
+                r.Add(queue.Peek());
+            }
+        }
+        if(r.Count==0){
+            r.Add(queue.Peek());
+        }
+        return r.ToArray();
     }
 }
 // @lc code=end
-
