f28da4b17ca6d305a8fd22935783c1b09111db30
Felix <abcd8992689926@gmail.com>
2023-03-14 22:58:23 +0800

    Second Try

--- 503.next-greater-element-ii.cs
+++ 503.next-greater-element-ii.cs
@@ -8,25 +8,20 @@
 public class Solution {
     private Queue<int> queue=new Queue<int>();
     public int[] NextGreaterElements(int[] nums) {
-        var r=new List<int>();
-        queue.Enqueue(nums[0]);
-        for(var x=1; x<nums.Length; ++x){
-            while(queue.Peek()<nums[x]){
-                queue.Dequeue();
+        var l=nums.Length;
+        var stack=new Stack<int>();
+        int[] r=new int[l];
+        Array.Fill(r, -1);
+        for(var x=0; x<l*2; ++x){
+            var i=x%l;
+            while(stack.Count>0&&r[stack.Peek()]<r[i]){
+                r[stack.Pop()]=nums[i];
             }
-            if(queue.Count==0){
-                queue.Enqueue(nums[x]);
+            if(i<l){
+                stack.Push(i);
             }
-            if(x!=nums.Length-1&&nums[x]>nums[x+1]){
-                r.Add(-1);
-            }else{
-                r.Add(queue.Peek());
-            }
+            return r;
         }
-        if(r.Count==0){
-            r.Add(queue.Peek());
-        }
-        return r.ToArray();
     }
 }
 // @lc code=end
